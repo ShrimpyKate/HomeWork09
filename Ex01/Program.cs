5dbc0309100759c9b02ca35e9e9cbfde81753d7d
Kate <katekrupnik@gmail.com>
2023-01-21 16:50:38 +0300

    code for ex01

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int GetNumberByUser();
+{
+  Console.WriteLine("Введите значение n: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+  return num;
+}
+
+
+void Numbers(int n)
+{
+  Console.WriteLine(n);
+  if (n == 1) return;
+  n--;
+  Numbers(n);
+}
+
+
+
+int numb = GetNumberByUser();
+Numbers(numb);
