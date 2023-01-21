a643ac280cd699af219cd9c74a9ed9532e1dcc6b
Kate <katekrupnik@gmail.com>
2023-01-21 23:26:41 +0300

    code for ex02

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -8,21 +8,33 @@
 
 int GetNumberByUser()
 {
-  Console.Write("Введите значение n: ");
+  Console.Write("Введите значение : ");
   int num = Convert.ToInt32(Console.ReadLine());
   return num;
 }
 
 void SumOfNumbersBetweenMandN(int M, int N)
 {
-  if (N != M) Console.Write($"{N}, ");
-  else Console.Write($"{N}.");
+  int sum = 0;
+  int temp=0;
+  
+  for (int i = 0; i < M; i++)
+  {
+  // if (N != M) Console.Write($"{N}, ");
+  // else Console.WriteLine($"{N}.");
+  temp+=N;
+  Console.WriteLine(sum);
   if (N == M) return;
   N++;
-  SumOfNumbersBetweenMandN+
-  SumOfNumbersBetweenMandN(M,N);
+  sum= temp+N;
+  
+  }
+  
+  SumOfNumbersBetweenMandN(M, N);
+  
 }
+Console.WriteLine();
 
 int n = GetNumberByUser();
 int m = GetNumberByUser();
-SumOfNumbersBetweenMandN(m, n);
+SumOfNumbersBetweenMandN(m,n);
